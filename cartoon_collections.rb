d6ed057d9f069e2e8b66c226ee66c459b75612c7
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-28 23:47:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,8 @@ end
 
 def summon_captain_planet(array)
   array = []
-  array.map {|call| call.cap
- 
-end
+  array.map {|call| call.capitalize.join("!")
+ end
 
 def long_planeteer_calls# code an argument here
   # Your code here
