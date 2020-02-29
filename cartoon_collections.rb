3e02336cddbd1128689798482edd1c71ba6588ca
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-29 00:11:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(array)
 end
 end
 
-def find_the_cheese
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
